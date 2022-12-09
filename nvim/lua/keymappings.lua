341099146839c1f9997762987f440567bac6e1f1
Agustina Pesce <pesce.agustina@gmail.com>
2022-12-08 16:16:24 -0800

    Fix typo

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -3,15 +3,16 @@
 
 local keymap = vim.api.nvim_set_keymap
 
+
 vim.g.mapleader = ' '  -- Remap space as leader key
 
-keymap('n', '<c-s>', ':w<CR>',{}) -- Save with ctrl+s
-keymap('i', '<c-s>', '<Esc>:w<CR>a',{}) -- Sace with ctrl+s
-keymap('v', '<c-c>', '"+y', {}) -- Map "+y in order to copy to clipboard
+keymap('n', '<C-s>', ':w<CR>',{}) -- Save with ctrl+s
+keymap('i', '<C-s>', '<Esc>:w<CR>a',{}) -- Sace with ctrl+s
+keymap('v', '<C-c>', '"+y', {}) -- Map "+y in order to copy to clipboard
 
 -- Split keyboard shortcuts
 local opts = {noremap = true}
-keymap('n', '<c-j>', '<c-w>j',opts)  -- Up
-keymap('n', '<c-h>', '<c-w>h',opts)  -- Left
-keymap('n', '<c-k>', '<c-w>k',opts)  -- Down
-keymap('n', '<c-l>', '<c-w>l',opts)  -- Right
+keymap('n', '<C-j>', '<c-w>j',opts)  -- Up
+keymap('n', '<C-h>', '<c-w>h',opts)  -- Left
+keymap('n', '<C-k>', '<c-w>k',opts)  -- Down
+keymap('n', '<C-l>', '<c-w>l',opts)  -- Right
